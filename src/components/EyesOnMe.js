10784427b0bb29fe50d2e7a0effab607e56ce459
+ <+>
2024-08-17 12:58:10 +0300

    test completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    handleBlur=()=>{
+        console.log('Hey! Eyes on me!');
+    }
+    handleFocus=()=>{
+        console.log('Good!');
+    }
+  return (
+    <button
+    onBlur={handleBlur}
+    onFocus={handleFocus}
+    >
+        Eyes on me
+    </button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    handleChange=()=>{
+        console.log('Entering password...');
+    }
     return (
-        <div></div>
+        <input 
+        type="password"
+        onChange={handleChange}
+        />
     )
 }
 
